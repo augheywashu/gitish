679252f25e636174a9241d8d3853b154aa546c57
John Aughey <jha@aughey.com>
2009-02-19 11:05:10 -0600

    Renaming blobstoreremote.rb to remote.rb

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # There should be better tests than this.
 
 export LOG_IO=1
-export GITISH_REMOTE='ruby blobstoreremote.rb'
+export GITISH_REMOTE='ruby remote.rb'
 export CRYPT_KEY='jklfdsajlkfdsaijofdsaoi'
 
 rm -rf store/* *.db restore/*
