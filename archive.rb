781a740762566eb95b7e78f92a11fdace10aa9fe
John Aughey <jha@aughey.com>
2009-02-18 22:12:46 -0600

    Fixing how chunk sizes are computed. Adding some debug messages for progress logging.

--- archive.rb
+++ archive.rb
@@ -1,6 +1,8 @@
 require 'digest/sha1'
 
 class Archive
+  CHUNKSIZE=1048576.0
+
   def initialize(blobstore)
     @blobstore = blobstore
   end
@@ -10,15 +12,21 @@ class Archive
   end
 
   def write_file(path)
-    STDERR.puts "Writing file #{path}"
+    size = File.stat(path).size
+    STDERR.puts "Writing file #{path} (#{size} bytes)"
+
+    chunks = (size / CHUNKSIZE).ceil
 
     shas = []
+    chunk = 1
     File.open(path,'r') do |f|
       until f.eof?
-        data = f.read(1048576)
+        data = f.read(CHUNKSIZE)
         sha = Digest::SHA1.hexdigest(data)
+        STDERR.puts "Writing chunk #{chunk} of #{chunks}"
         @blobstore.write(data,sha) unless @blobstore.has_sha?(sha)
         shas << sha
+        chunk += 1
       end
     end
     shas.join(',')
